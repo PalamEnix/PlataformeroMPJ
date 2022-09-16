0e25e5c5452c4546306f0ac140d625187cea5db2
PalamEnix <marco.palma@ucr.ac.cr>
2022-09-15 19:10:28 -0600

    primeras animaciones

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,33 +6,60 @@ public class Player : MonoBehaviour
 {
     public float speed;
 
+    public float jumpForce;
+
     private Rigidbody2D rigidBody2D;
 
     private float horizontal;
+
+    private bool isGrounded;
+
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal") * speed;
-        if(horizontal < 0.0f)
+        if (horizontal < 0.0f)
         {
-            transform.localScale = new Vector2(-1.0f, 1.0f);
+            transform.localScale = new Vector2(-20.0f, 20.0f);
         }
         else if (horizontal > 0.0f)
         {
-            transform.localScale = new Vector2(1.0f, 1.0f);
+            transform.localScale = new Vector2(20.0f, 20.0f);
         }
+
+        animator.SetBool("isRunning", horizontal != 0.0f);
+
+        if(Physics2D.Raycast(transform.position, Vector2.down, 1.1f))
+        {
+            isGrounded = true;
+        } else
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rigidBody2D.AddForce(Vector2.up * jumpForce);
     }
 
     private void FixedUpdate()
     {
-        rigidBody2D.velocity = new Vector2(horizontal, rigidBody2D.velocity.y); 
+        rigidBody2D.velocity = new Vector2(horizontal, rigidBody2D.velocity.y);
     }
 
 }
